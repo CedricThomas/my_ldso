94cdd9d5e63e2d5515067134c5f642632099451e
Cédric Thomas <thomascedric67@gmail.com>
2026-05-01 14:26:48 +0200

    fix: merge auxv and direct loading paths in ldso_main
    When running ./ld.so ./binary, the auxv belongs to ld.so itself,
    not the target binary. Detect direct mode by checking if argv[0]
    ends with "ld.so" and there are extra arguments.
    
    In direct mode, load the target binary from path and adjust the
    stack so the target sees correct argc/argv. Remove the commented-out
    direct loading code that was never integrated.

--- ldso/main.c
+++ ldso/main.c
@@ -37,12 +37,30 @@ void ldso_main(u64 *stack)
         print_auxv(auxv);
     }
 
-    auxv_info_t auxv_info;
-    load_auxv_info(&auxv_info, auxv);
-
     dso_t bin;
+    auxv_info_t auxv_info = {0};
 
-    load_dso_from_auxv(&bin, &auxv_info,  argv[0], argv[0]);
+    /* Detect direct mode: ./ld.so ./target_bin ...
+     * argv[0] ends with "ld.so" and there are extra args */
+    int direct_mode = 0;
+    if (argc > 1) {
+        size_t slen = 0;
+        while (argv[0][slen]) slen++;
+        if (slen >= 5) {
+            const char *suffix = argv[0] + slen - 5;
+            if (suffix[0] == 'l' && suffix[1] == 'd' && suffix[2] == '.' &&
+                suffix[3] == 's' && suffix[4] == 'o')
+                direct_mode = 1;
+        }
+    }
+
+    if (direct_mode) {
+        argv[0] = argv[1];
+        load_dso_from_path(&bin, argv[0], argv[0]);
+    } else {
+        load_auxv_info(&auxv_info, auxv);
+        load_dso_from_auxv(&bin, &auxv_info, argv[0], argv[0]);
+    }
 
     linked_list_t dependencies;
     linked_list_init(&dependencies);
@@ -58,60 +76,20 @@ void ldso_main(u64 *stack)
         print_link_map(&dependencies);
         _exit(0);
     }
-    
+
     linked_list_for_each(&dependencies, n) {
         relocate_dso(&n->data, &dependencies);
     }
 
-    if (auxv_info.base != 0) {
+    if (direct_mode) {
+        /* After argv[0] = argv[1], stack[2] is already the target name.
+         * Just fix argc and shift the NULL terminator left by 1. */
+        u64 new_argc = argc - 1;
+        /* Move NULL from position argc+2 to argc+1 */
+        stack[1 + new_argc + 1] = stack[1 + argc + 1];  /* shift NULL left */
+        stack[1] = new_argc;
+        jmp_to_usercode((u64)dso_resolve_ptr(&bin, bin.ehdr->e_entry), (u64)&stack[1]);
+    } else {
         jmp_to_usercode(auxv_info.entry, (u64)stack);
     }
-
-    if (argc <= 1) {
-        exit_with_error("missing program name\n");
-    }
-
-    exit_with_error("direct loading not implemented yet\n");
 }
-
-
-// void ldso_main(u64 *stack)
-// {
-
-//     int argc = *stack;
-//     char **argv = (void *)&stack[1];
-//     char **envp = argv + argc + 1;
-
-//     set_program_name(argv[0]);
-
-//     if (argc <= 1) {
-//         exit_with_error("missing program name\n");
-//     }
-
-//     dso_t bin;
-
-//     load_dso_from_path(&bin, argv[1],  argv[1]);
-
-//     linked_list_t dependencies;
-//     linked_list_init(&dependencies);
-//     linked_list_append(&dependencies, bin);
-//     resolve_dependencies_recursive(
-//         &dependencies,
-//         &bin,
-//         envp
-//     );
-
-//     // If LD_TRACE_LOADED_OBJECTS is set, print the loaded objects statuses
-//     if (check_ld_trace_loaded_objects(envp)) {
-//         print_link_map(&dependencies);
-//         _exit(0);
-//     }
-    
-//     linked_list_for_each(&dependencies, n) {
-//         relocate_dso(&n->data, &dependencies);
-//     }
-
-//     jmp_to_usercode((u64)dso_resolve_ptr(&bin, bin.ehdr->e_entry), (u64)stack);
-
-//     exit_with_error("direct loading not implemented yet\n");
-// }
